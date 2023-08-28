54e54949c964012908621d9abdd0e6d3e419f0ab
Aza <user.@mail.com>
2023-08-28 18:58:59 +0500

    save

---
+++ assets/MoviesPage-8531c062.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Catalog-8841da0d.js";import{o as a,c as o,a as s}from"./index-8209fc82.js";const t={class:"main"},i={__name:"MoviesPage",setup(c){return(_,r)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
